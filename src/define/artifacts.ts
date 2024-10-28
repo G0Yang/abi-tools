1636f92652c8cd513504e1835a4a93dafbba3d19
ender <ender35841@gmail.com>
2024-10-28 23:23:40 +0900

    feat: add solidity artifact.

---
+++ src/define/artifacts.ts
@@ -0,0 +1,29 @@
+import * as ERC20 from "@/public/artifacts/ERC20.json"
+import * as ERC20Burnable from "@/public/artifacts/ERC20Burnable.json"
+import * as ERC20Capped from "@/public/artifacts/ERC20Capped.json"
+import * as ERC20FlashMint from "@/public/artifacts/ERC20FlashMint.json"
+import * as ERC20Pausable from "@/public/artifacts/ERC20Pausable.json"
+import * as ERC20Permit from "@/public/artifacts/ERC20Permit.json"
+import * as ERC20Votes from "@/public/artifacts/ERC20Votes.json"
+import * as ERC20Wrapper from "@/public/artifacts/ERC20Wrapper.json"
+import * as ERC1363 from "@/public/artifacts/ERC1363.json"
+import * as ERC1363Utils from "@/public/artifacts/ERC1363Utils.json"
+import * as ERC4626 from "@/public/artifacts/ERC4626.json"
+import * as SafeERC20 from "@/public/artifacts/SafeERC20.json"
+
+const artifacts = {
+    ERC20,
+    ERC20Burnable,
+    ERC20Capped,
+    ERC20FlashMint,
+    ERC20Pausable,
+    ERC20Permit,
+    ERC20Votes,
+    ERC20Wrapper,
+    ERC1363,
+    ERC1363Utils,
+    ERC4626,
+    SafeERC20,
+}
+
+export default artifacts
