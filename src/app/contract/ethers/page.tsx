7bb16145329457f16971d56498cb7697165cb46a
ender <ender35841@gmail.com>
2024-11-04 22:39:02 +0900

    fix: ethers 페이지 접속시 signer, contractName 필드 미표기 이슈 수정.

--- src/app/contract/ethers/page.tsx
+++ src/app/contract/ethers/page.tsx
@@ -1,11 +1,20 @@
 'use client'
 
 import * as React from 'react'
-import { useContractState } from '@/src/define/useLocalStorageState'
+import { useContractState, useRpcUrlState } from '@/src/define/useLocalStorageState'
 import ContractPenal from '@/src/components/contractPenal'
+import { useProvider } from '@/src/store/provider'
+import { useEffect } from 'react'
 
 export default function EthersPage() {
+  const { setRpcUrl, reset } = useProvider()
+  const [rpcUrl] = useRpcUrlState()
   const [contracts] = useContractState()
+
+  useEffect(() => {
+    if (rpcUrl) setRpcUrl(rpcUrl)
+    else reset()
+  }, [])
 
   if (!contracts) return <></>
 
